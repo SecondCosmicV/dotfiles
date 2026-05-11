b05ebc2229ad7a5719628381566bb87b949af6a4
SecondCosmicV <secondcosmicv@gmail.com>
2026-05-11 16:31:25 +0200

    add presetup script

--- postsetup.sh
+++ postsetup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -x
 dconf write /org/gtk/settings/file-chooser/show-hidden true
+nmcli connection import type wireguard file ~/stuff/vpn/vpn.conf
+until ping -c1 google.com; do
+    sleep 1
+done
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.signal.Signal
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.spotify.Client
-nmcli connection import type wireguard file /mnt/data/stuff/vpn/vpn.conf
 
