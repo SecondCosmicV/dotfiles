fd342f0627fdfbc3725da1cad519c492fd89a137
SecondCosmicV <secondcosmicv@gmail.com>
2026-05-22 18:54:16 +0200

    extend ii

--- postsetup.sh
+++ postsetup.sh
@@ -2,10 +2,6 @@
 
 set -x
 dconf write /org/gtk/settings/file-chooser/show-hidden true
-nmcli connection import type wireguard file ~/stuff/vpn/vpn.conf
-until ping -c1 google.com; do
-    sleep 1
-done
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.signal.Signal
 flatpak install flathub com.discordapp.Discord
--- presetup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-ln -s /mnt/data/stuff ~/
-
