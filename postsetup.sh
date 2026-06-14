b579ec80d011e4c4b4ba4f1b90cbc351c71b97b4
SecondCosmicV <secondcosmicv@gmail.com>
2026-06-14 16:55:42 +0200

    create roots in adv

--- postsetup.sh
+++ postsetup.sh
@@ -7,4 +7,21 @@ flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.spotify.Client
 cd ~/stuff/secrets
 stow -R -v -t ~ .
+mkdir -p ~/stuff/roots
+rm -f ~/stuff/roots/yt-dlp
+guix shell -r ~/stuff/roots/yt-dlp \
+    nss-certs \
+    openssl \
+    python-yt-dlp \
+    -- \
+    true
+rm -f ~/stuff/roots/cursor
+guix shell -r ~/stuff/roots/cursor --container --emulate-fhs \
+    adwaita-icon-theme \
+    bash \
+    coreutils \
+    cursor \
+    git \
+    -- \
+    true
 
